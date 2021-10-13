4bf93847ce7c65fa19cd3441de3c1e60b44a04b4
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2021-10-13 16:49:16 +0200

    øving6

--- IT2805-Webtek/øving6/tax.js
+++ IT2805-Webtek/øving6/tax.js
@@ -2,6 +2,6 @@ inputTax = document.querySelector('#tax');
 income = document.querySelector('#income');
 wealth = document.querySelector('#wealth');
 document.addEventListener("keyup", () => {
-    let tax = (0.35 * income.value) + (0.25 * wealth.value);
+    let tax = (0.35 * Number(income.value)) + (0.25 * Number(wealth.value));
     inputTax.value = tax;
 });
