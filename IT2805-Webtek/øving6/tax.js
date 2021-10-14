efde5bb86dfa68e51cc4f72e219a9ad8a8767de3
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2021-10-14 12:56:06 +0200

    tax.js FERDIG

--- IT2805-Webtek/øving6/tax.js
+++ IT2805-Webtek/øving6/tax.js
@@ -2,7 +2,7 @@ inputTax = document.querySelector("#tax");
 income = document.querySelector("#income");
 wealth = document.querySelector("#wealth");
 
-document.addEventListener("keyup", () => {
+document.addEventListener("change", () => {
     let tax = 0.35 * Number(income.value) + 0.25 * Number(wealth.value);
     inputTax.value = tax;
 });
