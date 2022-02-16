cb041f22ac4a48d0d76a93bcab803a574d2ad171
havardnyboe <havard.nyboe@gmail.com>
2022-02-16 15:23:35 +0100

    fix øving 4

--- TDT4102-C++/øving4/mastermind.cpp
+++ TDT4102-C++/øving4/mastermind.cpp
@@ -38,43 +38,43 @@ void playMastermind() {
 }
 
 void playMastermindVisual() {
-    // int numberOfGuesses{6};
+    int numberOfGuesses{6};
 
-    // std::string code{randomizeString(SIZE, 'A', 'A' + LETTERS - 1)};
-    // std::string guess;
+    std::string code{randomizeString(SIZE, 'A', 'A' + LETTERS - 1)};
+    std::string guess;
 
     MastermindWindow mwin{900, 20, winW, winH, SIZE, "Mastermind"};
-    mwin.wait_for_close();
     mwin.draw_rectangle(Point{10, 10}, padX * 10, padY * 10);
-    // mwin.setCodeHidden(false);
+    mwin.setCodeHidden(false);
 
-    // guess = mwin.getInput(SIZE, 'A', 'A' + LETTERS - 1);
-    // addGuess(mwin, guess, 'A');
-    // mwin.redraw();
-    // --numberOfGuesses;
+    guess = mwin.getInput(SIZE, 'A', 'A' + LETTERS - 1);
+    addGuess(mwin, guess, 'A');
+    mwin.redraw();
+    --numberOfGuesses;
 
-    // while (checkCharactersAndPosition(code, guess) < SIZE || numberOfGuesses > 0) {
-    //     clear();
-    //     std::cout << "Du gjettet: " << guess
-    //               << std::endl
-    //               << "Du hadde " << checkCharacters(code, guess) << " bokstaver riktig"
-    //               << std::endl
-    //               << "Du hadde " << checkCharactersAndPosition(code, guess) << " boksstaver riktig og på riktig plass"
-    //               << std::endl;
-    //     if (numberOfGuesses > 0) {
-    //         std::cout << "Du har " << numberOfGuesses << " forsøk igjen" << std::endl;
-    //     } else {
-    //         std::cout << "Du har ikke flere forsøk igjen. Koden var: " << code << std::endl;
-    //         break;
-    //     }
-    //     guess = mwin.getInput(SIZE, 'A', 'A' + LETTERS - 1);
-    //     mwin.redraw();
-    //     --numberOfGuesses;
-    //     if (guess == code) {
-    //         clear();
-    //         std::cout << "Du vant! Koden var: " << code << std::endl;
-    //     }
-    // }
+    while (checkCharactersAndPosition(code, guess) < SIZE || numberOfGuesses > 0) {
+        clear();
+        std::cout << "Du gjettet: " << guess
+                  << std::endl
+                  << "Du hadde " << checkCharacters(code, guess) << " bokstaver riktig"
+                  << std::endl
+                  << "Du hadde " << checkCharactersAndPosition(code, guess) << " boksstaver riktig og på riktig plass"
+                  << std::endl;
+        if (numberOfGuesses > 0) {
+            std::cout << "Du har " << numberOfGuesses << " forsøk igjen" << std::endl;
+        } else {
+            std::cout << "Du har ikke flere forsøk igjen. Koden var: " << code << std::endl;
+            break;
+        }
+        guess = mwin.getInput(SIZE, 'A', 'A' + LETTERS - 1);
+        mwin.redraw();
+        --numberOfGuesses;
+        if (guess == code) {
+            clear();
+            std::cout << "Du vant! Koden var: " << code << std::endl;
+        }
+    }
+    mwin.wait_for_close();
 }
 
 int checkCharactersAndPosition(std::string code, std::string guess) {
