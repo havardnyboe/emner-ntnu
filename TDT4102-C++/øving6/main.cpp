eb8b1d89240b5603d87a3752e8ee2f16ec434daf
havardnyboe <havard.nyboe@gmail.com>
2022-02-24 11:27:56 +0100

    øving 6

--- TDT4102-C++/øving6/main.cpp
+++ TDT4102-C++/øving6/main.cpp
@@ -1,9 +1,8 @@
 #include "std_lib_facilities.h"
 
 void writeInputToFile(string filename) {
-    std::ofstream outfile;
-    outfile.open(filename);
-    std::string input;
+    ofstream outfile(filename, ios_base::app);
+    string input;
     cout << "Write a word and press enter. Type 'quit' to quit.\n";
     while (cin >> input) {
         if (input == "quit")
@@ -12,7 +11,30 @@ void writeInputToFile(string filename) {
     }
 }
 
+void readFileAndAddLineNumbers(string filename) {
+    ifstream infile(filename);
+    ofstream outfile("output.txt");
+    string line;
+    if (!infile) {
+        cout << "File not found.\n";
+        return;
+    }
+    int line_number = 1;
+    while (std::getline(infile, line)) {
+        outfile << line_number << ": " << line << '\n';
+        ++line_number;
+    }
+}
+
+void countCharactersInFile(string filename) {
+    array<map<char, int>, 26> charCount;
+    for (char c{'a'}; c <= 'z'; c++) {
+        charCount.at(c - 'a').insert({c, 0});
+    }
+}
+
 int main() {
     writeInputToFile("opg1a.txt");
+    readFileAndAddLineNumbers("opg1a.txt");
     return 0;
 }
