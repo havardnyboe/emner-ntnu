17335df73fbab8b064c8d5ddf4056326d1745648
havardnyboe <havard.nyboe@gmail.com>
2022-09-15 23:45:54 +0200

    added example from lecture
    makes LED0 turn on

--- TDT4160-DatDig/o1/o1.s
+++ TDT4160-DatDig/o1/o1.s
@@ -8,7 +8,16 @@
 	
 Start:
 
-    // Skriv din kode her...
+    LDR R0, =PORT_E
+	LDR R1, =PORT_SIZE
+	MUL R0, R0, R1
+	LDR R1, =GPIO_BASE
+	ADD R0, R0, R1
+
+	MOV R2, #1
+	LSL R2, R2, #LED_PIN
+	LDR R1, =GPIO_PORT_DOUTSET
+	STR R2, [R0, R1]
 
 
 NOP // Behold denne på bunnen av fila
