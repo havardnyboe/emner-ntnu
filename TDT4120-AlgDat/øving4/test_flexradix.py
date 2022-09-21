ca94dc7de3111760eb33735dbbd9fa4a3b37be1f
havardnyboe <havard.nyboe@gmail.com>
2022-09-21 11:09:13 +0200

    ikke ferdig

--- TDT4120-AlgDat/øving4/test_flexradix.py
+++ TDT4120-AlgDat/øving4/test_flexradix.py
@@ -5,10 +5,28 @@
 def char_to_int(char):
     return ord(char) - 97
 
+def counting_sort(A, position):
+    k = char_to_int("z")
+    B = [0]*len(A)
+    C = [0]*26 
+    for i in A:
+        C[char_to_int(A[i][position])] += 1 # Teller instanser av hvert tall
+    for i in range(1,k):
+        C[i] += C[i-1]
+    for i in range(len(A),0,-1): # Går baklengs for å gjøre algoritmen stabil
+        num = char_to_int(A[i][position])
+        B[C[num]-1] = num
+        C[num] -= 1
+    return B
 
 def flexradix(A, n, d):
-    # Skriv koden din her
-    pass
+    for i in range(n):
+        for j in range(d):
+            A[i] += " "*(d-len(A[i]))
+
+    for i in range(d-1, -1, -1):
+        counting_sort(A, i)
+
 
 
 tests = (
