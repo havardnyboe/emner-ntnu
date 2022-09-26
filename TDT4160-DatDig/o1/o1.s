e87e05567e9c46c7e352572d4f8b5c3f83e21a10
havardnyboe <havard.nyboe@gmail.com>
2022-09-26 19:26:15 +0200

    la til kommentarer

--- TDT4160-DatDig/o1/o1.s
+++ TDT4160-DatDig/o1/o1.s
@@ -8,17 +8,15 @@
 	
 Start:
 
-    LDR R0, =PORT_E
-	LDR R1, =PORT_SIZE
-	MUL R0, R0, R1
-	LDR R1, =GPIO_BASE
-	ADD R0, R0, R1
+	LDR R0, =PORT_E    // finner offset til PORT E
+	LDR R1, =PORT_SIZE // finner offstt til hele porten
+	MUL R0, R0, R1     // regner ut total offset til PORT E (avstanden mellom GPIO_BASE og PORT E i bits)
+	LDR R1, =GPIO_BASE // finner GPIO base-adresse
+	ADD R0, R0, R1     // laster R0 med adressen til PORT E
 
-	MOV R2, #1
-	LSL R2, R2, #LED_PIN
-	LDR R1, =GPIO_PORT_DOUTSET
-	STR R2, [R0, R1]
-
+	MOV R2, #1                 // laster tallet 1 til register R2
+	LSL R2, R2, #LED_PIN       // bit-shifter 0b00000001 til 0b00000100
+	LDR R1, =GPIO_PORT_DOUTSET // finner offset til "Set data output"
+	STR R2, [R0, R1]           // lagre R2 (0b00000100) på minnelokasjon R0 + R1 (PORT E address + DOUTSET) 
 
 NOP // Behold denne på bunnen av fila
-
