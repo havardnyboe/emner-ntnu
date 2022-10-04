47108f2b95da0c0170b05ac50c83016748d250c4
havardnyboe <havard.nyboe@gmail.com>
2022-10-04 19:56:10 +0200

    det funker?! :o

--- TDT4160-DatDig/o1/o1.s
+++ TDT4160-DatDig/o1/o1.s
@@ -8,15 +8,51 @@
 	
 Start:
 
-	LDR R0, =PORT_E    // finner offset til PORT E
-	LDR R1, =PORT_SIZE // finner offstt til hele porten
-	MUL R0, R0, R1     // regner ut total offset til PORT E (avstanden mellom GPIO_BASE og PORT E i bits)
-	LDR R1, =GPIO_BASE // finner GPIO base-adresse
-	ADD R0, R0, R1     // laster R0 med adressen til PORT E
+	LDR R1, = BUTTON_PORT  // finner offset til PORT B
+	LDR R0, =PORT_SIZE 	   // finner offstt til hele porten
+	MUL R1, R1, R0
+	LDR R0, =GPIO_BASE
+	ADD R1, R1, R0
 
-	MOV R2, #1                 // laster tallet 1 til register R2
-	LSL R2, R2, #LED_PIN       // bit-shifter 0b00000001 til 0b00000100
-	LDR R1, =GPIO_PORT_DOUTSET // finner offset til "Set data output"
-	STR R2, [R0, R1]           // lagre R2 (0b00000100) på minnelokasjon R0 + R1 (PORT E address + DOUTSET) 
+	LDR R3, =GPIO_PORT_DIN // finner offset til "Data input"
+	ADD R3, R3, R1
+
+	LDR R1, =LED_PORT  // finner offset til PORT E
+	LDR R0, =PORT_SIZE // finner offstt til hele porten
+	MUL R1, R1, R0     // regner ut total offset til PORT E (avstanden mellom GPIO_BASE og PORT E i bits)
+	LDR R0, =GPIO_BASE // finner GPIO base-adresse
+	ADD R1, R1, R0     // laster R1 med adressen til PORT E
+
+	LDR R4, =GPIO_PORT_DOUTSET // finner offset til "Set data output"
+	ADD R4, R4, R1
+	LDR R5, =GPIO_PORT_DOUTCLR // finner offset til "Clear data output"
+	ADD R5, R5, R1
+	
+	MOV R10, #1                    // laster tallet 1 til register R10
+	LSL R10, R10, LED_PIN		   // bit-shifter til LED pinen (?)
+	
+	B LOOP
+
+LOOP:
+
+	LDR R1, [R3]
+	MOV R0, #1
+	LSL R0, R0, BUTTON_PIN
+	AND R2, R1, R0
+	CMP R2, 0
+
+	BEQ TURNON
+
+	B TURNOFF
+
+TURNON:
+
+	STR R10, [R4]
+	B LOOP
+
+TURNOFF:
+
+	STR R10, [R5]
+	B LOOP
 
 NOP // Behold denne på bunnen av fila
