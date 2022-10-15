a597ef1bdd05b4cddcc6276e2bffbcf7b8ffc286
havardnyboe <havard.nyboe@gmail.com>
2022-10-15 17:29:15 +0200

    starta på øving

--- TDT4160-DatDig/o2/o2.s
+++ TDT4160-DatDig/o2/o2.s
@@ -9,7 +9,35 @@
 	
 Start:
 
-    // Skriv din kode her...
+    LDR R10, =tenths
+    LDR R11, =seconds
+    LDR R12, =minutes
+
+    MOV R0, 0b0101
+
+    B LOOP
+
+LOOP:
+
+    STR R0, [R10]
+    STR R0, [R11]
+    STR R0, [R12]
+
+    B LOOP
+
+
+BEGIN_TIMER:
+
+
+
+    B BEGIN_TIMER
+
+
+PAUSE_TIMER:
+
+
+
+    B PAUSE_TIMER
 
 
 NOP // Behold denne på bunnen av fila
