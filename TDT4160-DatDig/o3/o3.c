a7b23e98fc5bb3a6333cd1b83fd5fa9f82cbecc8
havardnyboe <havard.nyboe@gmail.com>
2022-10-31 23:33:35 +0100

    kan skru av og på LED

--- TDT4160-DatDig/o3/o3.c
+++ TDT4160-DatDig/o3/o3.c
@@ -2,30 +2,30 @@
 #include "gpio.h"
 #include "systick.h"
 
-/**************************************************************************//**
- * @brief Konverterer nummer til string 
+/******************************************************************************
+ * @brief Konverterer nummer til string
  * Konverterer et nummer mellom 0 og 99 til string
  *****************************************************************************/
 void int_to_string(char *timestamp, unsigned int offset, int i) {
     if (i > 99) {
-        timestamp[offset]   = '9';
-        timestamp[offset+1] = '9';
+        timestamp[offset] = '9';
+        timestamp[offset + 1] = '9';
         return;
     }
 
     while (i > 0) {
-	    if (i >= 10) {
-		    i -= 10;
-		    timestamp[offset]++;
-		
-	    } else {
-		    timestamp[offset+1] = '0' + i;
-		    i=0;
-	    }
+        if (i >= 10) {
+            i -= 10;
+            timestamp[offset]++;
+
+        } else {
+            timestamp[offset + 1] = '0' + i;
+            i = 0;
+        }
     }
 }
 
-/**************************************************************************//**
+/******************************************************************************
  * @brief Konverterer 3 tall til en timestamp-string
  * timestamp-argumentet må være et array med plass til (minst) 7 elementer.
  * Det kan deklareres i funksjonen som kaller som "char timestamp[7];"
@@ -47,10 +47,58 @@ void time_to_string(char *timestamp, int h, int m, int s) {
     int_to_string(timestamp, 4, s);
 }
 
-int main(void) {
-    init();
-    // Skriv din kode her...
-    
-    return 0;
+typedef struct {
+    volatile word CTRL;
+    volatile word MODEL;
+    volatile word MODEH;
+    volatile word DOUT;
+    volatile word DOUTSET;
+    volatile word DOUTCLR;
+    volatile word DOUTTGL;
+    volatile word DIN;
+    volatile word PINLOCKN;
+} gpio_port_map_t;
+
+typedef struct {
+    volatile gpio_port_map_t ports[6];
+    volatile word unused_space[10];
+    volatile word EXTIPSELL;
+    volatile word EXTIPSELH;
+    volatile word EXTIRISE;
+    volatile word EXTIFALL;
+    volatile word IEN;
+    volatile word IF;
+    volatile word IFS;
+    volatile word IFC;
+    volatile word ROUTE;
+    volatile word INSENSE;
+    volatile word LOCK;
+    volatile word CTRL;
+    volatile word CMD;
+    volatile word EM4WUEN;
+    volatile word EM4WUPOL;
+    volatile word EM4WUCAUSE;
+} gpio_map_t;
+
+#define LED_PIN 2
+#define LED_PORT GPIO_PORT_E
+
+// Globale variabler
+volatile gpio_map_t *GPIO_map;
+
+void lightLED(bool turnOn) {
+    if (turnOn)
+        GPIO_map->ports[LED_PORT].DOUTSET = 0b0001 << LED_PIN;
+    else
+        GPIO_map->ports[LED_PORT].DOUTCLR = 0b0001 << LED_PIN;
 }
 
+int main(void) {
+    init();
+
+    GPIO_map = (gpio_map_t *)GPIO_BASE;
+
+    lightLED(true);
+
+    return 0;
+}
