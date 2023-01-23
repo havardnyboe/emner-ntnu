61ce5ada7a6256b87754c6e06900fffb17559f92
havardnyboe <havard.nyboe@gmail.com>
2023-01-23 18:49:09 +0100

    task 1.2

--- TDT4186-OS/lab1/hello.c
+++ TDT4186-OS/lab1/hello.c
@@ -1,12 +1,7 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        printf("Hello %s\n", argv[1]);
-    } else {
-        printf("Hello unknown user...\n");
-    }
+    (argc > 1) ? printf("Hello %s, nice to meet you!\n", argv[1]) : printf("Hello World\n");
     return 0;
 }
